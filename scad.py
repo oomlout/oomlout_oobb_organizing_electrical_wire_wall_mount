8b8299f6d088aaba1e25608718d28ecdc2e589ed
Oom Lout <aaron@oomlout.com>
2024-10-22 17:48:37 +0100

    added glands

--- scad.py
+++ scad.py
@@ -12,6 +12,8 @@ def make_scad(**kwargs):
     # save_type variables
     if True:
         filter = ""
+        filter = "gland"
+
         #filter = "3_wire_10_spacing_6_wire"
         #filter = "4_wire"
         #filter = "ninety_degree"
@@ -115,6 +117,24 @@ def make_scad(**kwargs):
             part["name"] = f"base_{wire_count}_wire_{wire_spacing}_spacing_{wire_diameter}_wire_ninety_degrees"
             parts.append(part)
         
+#gland
+    ids = [6,4.5,4,3]
+    ods = [8]
+    
+    for id in ids:
+        for od in ods:
+            part = copy.deepcopy(part_default)
+            p3 = copy.deepcopy(kwargs)
+            p3["thickness"] = 12
+            p3["width"] = 1
+            p3["height"] = 1
+            p3["wire_inner_diameter"] = id
+            p3["wire_outer_diameter"] = od
+            p3["thickness"] = 14
+            p3["extra"] = "gland"
+            part["kwargs"] = p3
+            part["name"] = f"gland_{od}_wire_outside_diameter_{id}_wire_inside_diameter"
+            parts.append(part)
 
         
     #make the parts
@@ -133,6 +153,8 @@ def get_base(thing, **kwargs):
     extra = kwargs.get("extra", "")
     if "ninety_degrees" in extra:
         get_base_ninety_degrees(thing, **kwargs)
+    elif "gland" in extra:
+        get_base_gland(thing, **kwargs)
     else:
         depth = kwargs.get("thickness", 4)
         prepare_print = kwargs.get("prepare_print", True)
@@ -290,6 +312,112 @@ def get_base(thing, **kwargs):
             #p3["m"] = "#"
             oobb_base.append_full(thing,**p3)
     return thing
+
+def get_base_gland(thing, **kwargs):     
+    width = kwargs.get("width", 2)
+    height = kwargs.get("height", 5)
+    pos = kwargs.get("pos", [0, 0, 0])
+    prepare_print = kwargs.get("prepare_print", True)
+    thickness = kwargs.get("thickness", 12)
+    depth = thickness
+    wire_inner_diameter = kwargs.get("wire_inner_diameter", 4)
+    wire_outer_diameter = kwargs.get("wire_outer_diameter", 8)
+
+    
+    wire_length = 1.5 * 15
+    inside_gap = 14 + 1
+
+    #add end plates
+    if True:
+        p3 = copy.deepcopy(kwargs)
+        p3["type"] = "p"
+        p3["shape"] = f"oobb_cylinder"    
+        p3["radius"] = depth/2
+        dep = (wire_length - inside_gap) / 2
+        p3["depth"] = dep
+        #p3["m"] = "#"
+        pos1 = copy.deepcopy(pos)            
+        pos1[2] += dep/2
+        pos1[1] += dep/2
+        pos11 = copy.deepcopy(pos1)
+        pos11[1] += wire_length/2 - dep/2
+        pos12 = copy.deepcopy(pos1)
+        pos12[1] += -(wire_length/2 - dep/2)
+        poss = [pos11,pos12]
+        p3["pos"] = poss
+        rot1 = [90,0,0]
+        p3["rot"] = rot1
+        p3["zz"] = "middle"
+        oobb_base.append_full(thing,**p3)
+        #add holes
+    
+    #wiremiddle_piese
+    if True:
+        p3 = copy.deepcopy(kwargs)
+        p3["type"] = "p"
+        p3["shape"] = f"oobb_cylinder"    
+        rad = (wire_outer_diameter-0.5)/2
+        p3["radius"] = rad
+        dep = wire_length
+        p3["depth"] = dep
+        #p3["m"] = "#"
+        pos1 = copy.deepcopy(pos)            
+        pos1[2] += dep/2
+        pos1[1] += dep/2
+        p3["pos"] = pos1
+        rot1 = [90,0,0]
+        p3["rot"] = rot1
+        p3["zz"] = "middle"
+        oobb_base.append_full(thing,**p3)
+        #add holes
+    
+
+    #add wire hole
+    if True:
+        p3 = copy.deepcopy(kwargs)
+        p3["type"] = "n"
+        p3["shape"] = f"oobb_cylinder"
+        p3["depth"] = wire_length 
+        p3["radius"] = wire_inner_diameter/2
+        #p3["m"] = "#"
+        pos1 = copy.deepcopy(pos)
+        p3["pos"] = pos1
+        pos1[2] += wire_length/2
+        pos1[1] += wire_length/2
+        rot = [90,0,0]
+        p3["rot"] = rot        
+        oobb_base.append_full(thing,**p3)
+    
+    
+
+
+    if prepare_print:
+        #put into a rotation object
+        components_second = copy.deepcopy(thing["components"])
+        return_value_2 = {}
+        return_value_2["type"]  = "rotation"
+        return_value_2["typetype"]  = "p"
+        pos1 = copy.deepcopy(pos)
+        pos1[0] += height*15 + 15
+        pos1[1] += height*15
+
+        pos1[2] += thickness
+        return_value_2["pos"] = pos1
+        return_value_2["rot"] = [180,0,0]
+        return_value_2["objects"] = components_second
+        
+        #thing["components"].append(return_value_2)
+        
+        #add slice # top
+        p3 = copy.deepcopy(kwargs)
+        p3["type"] = "n"
+        p3["shape"] = f"oobb_slice"
+        pos = [0,0,0]
+        p3["pos"] = pos
+        #p3["m"] = "#"
+        oobb_base.append_full(thing,**p3)
+
+
 
 def get_base_ninety_degrees(thing, **kwargs):     
     width = kwargs.get("width", 2)
